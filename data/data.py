9852844d127dd9ac90c1901ddf20a6fc2195ec8f
1Sardor <ikromjonovsardoronly@gmail.com>
2026-03-26 20:59:13 +0500

    a

--- data/data.py
+++ data/data.py
@@ -1,4 +1,3 @@
 # token = '8439688436:AAGIwsj1oNzGbMY_gGQ5rhgLwYf1ZPUJbdw'
-token = '6039319685:AAFQMypcpMvtNdPAGAEI_RaUX1_scb-ZrOk'
-# base_url = "https://crm.adnis.uz/api/bot/"
-base_url = "http://127.0.0.1:8000/api/bot/"
+base_url = "https://crm.adnis.uz/api/bot/"
+# base_url = "http://127.0.0.1:8000/api/bot/"
